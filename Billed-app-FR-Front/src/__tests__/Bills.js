27572b9d661f43daf7d10a491107ea8a6d800fce
Pasquinucci Sandrine <sandrine.pasquinucci@gmail.com>
2023-06-22 10:51:05 +0200

    Partie 5.1 réalisée

--- Billed-app-FR-Front/src/__tests__/Bills.js
+++ Billed-app-FR-Front/src/__tests__/Bills.js
@@ -49,21 +49,24 @@
  * @jest-environment jsdom
  */
 
-import { screen, waitFor } from "@testing-library/dom";
 import BillsUI from "../views/BillsUI.js";
-import { bills } from "../fixtures/bills.js";
-import { ROUTES, ROUTES_PATH } from "../constants/routes.js";
+import { screen, waitFor } from "@testing-library/dom";
+import { ROUTES, ROUTES_PATH } from "../constants/routes";
 import { localStorageMock } from "../__mocks__/localStorage.js";
+import mockStore from "../__mocks__/store";
+import { bills } from "../fixtures/bills";
+import router from "../app/Router";
 
-import router from "../app/Router.js";
-
-//pour faire fonctionner expect+toHaveClass
-//https://stackoverflow.com/questions/65723708/react-testing-library-typeerror-expect-tohavetextcontent-is-not-a-functi
+// //pour faire fonctionner expect+toHaveClass
+// //https://stackoverflow.com/questions/65723708/react-testing-library-typeerror-expect-tohavetextcontent-is-not-a-functi
 import "@testing-library/jest-dom/extend-expect";
 
 // //erreur 404 500
-import mockStore from "../__mocks__/store";
+// import mockStore from "../__mocks__/store";
 import Bills from "../containers/Bills.js";
+
+//ReferenceError: userEvent is not defined
+import userEvent from "@testing-library/user-event";
 
 jest.mock("../app/store", () => mockStore);
 
@@ -108,37 +111,76 @@ describe("Given I am connected as an employee", () => {
     });
   });
 });
-//Point 5.2.2 [Bug report] - Ajout des tests unitaires et d'intégration - Composant container/Bills
-describe("Given I am a user connected as an employee", () => {
-  describe("When I click on icon-eye", () => {
-    describe("When I navigate to Bills", () => {
-      // Il faut que le titre Mes notes de frais apparaisse bien sur la page
-      test("Then the page show", async () => {
-        const onNavigate = (pathname) => {
-          document.body.innerHTML = ROUTES({ pathname });
-        };
-        Object.defineProperty(window, "localStorage", {
-          value: localStorageMock,
-        });
-        window.localStorage.setItem(
-          "user",
-          JSON.stringify({
-            type: "Employee",
-          })
-        );
-        new Bills({
-          document,
-          onNavigate,
-          store: null,
-          localStorage: window.localStorage,
-        });
-        document.body.innerHTML = BillsUI({ data: bills });
-        await waitFor(() => screen.getByText("Mes notes de frais"));
-        expect(screen.getByText("Mes notes de frais")).toBeTruthy();
-      });
+//Point 5.2.1 [Bug report] - Ajout des tests unitaires et d'intégration - Composant container/Bills
+// Vérifie si la modale du justificatif apparait
+describe("When I click on the eye of a bill", () => {
+  test("Then a modal must appear", async () => {
+    const onNavigate = (pathname) => {
+      document.body.innerHTML = ROUTES({ pathname });
+    };
+    Object.defineProperty(window, "localStorage", { value: localStorageMock });
+    window.localStorage.setItem(
+      "user",
+      JSON.stringify({
+        type: "Employee",
+      })
+    );
+    const NEWBILLS = new Bills({
+      document,
+      onNavigate,
+      store: null,
+      localStorage: window.localStorage,
+    });
+    document.body.innerHTML = BillsUI({ data: bills });
+    const handleClickIconEye = jest.fn((icon) =>
+      NEWBILLS.handleClickIconEye(icon)
+    );
+    const iconEye = screen.getAllByTestId("icon-eye");
+    $.fn.modal = jest.fn(() => modaleFile.classList.add("show"));
+    iconEye.forEach((icon) => {
+      icon.addEventListener("click", handleClickIconEye(icon));
+      userEvent.click(icon);
+      expect(handleClickIconEye).toHaveBeenCalled();
     });
   });
 });
+describe('When click on button "Nouvelle note de frais"', () => {
+  test("Then handleClickNewBill is called", () => {
+    //RangeError: Maximum call stack size exceeded
+    let NEWBILLS;
+
+    const onNavigate = (pathname) => {
+      document.body.innerHTML = ROUTES({ pathname });
+    };
+
+    Object.defineProperty(window, "localStorage", {
+      value: localStorageMock,
+    });
+    window.localStorage.setItem(
+      "user",
+      JSON.stringify({
+        type: "Employee",
+      })
+    );
+
+    NEWBILLS = new Bills({
+      document,
+      onNavigate,
+      store: mockStore,
+      localStorage: window.localStorage,
+    });
+
+    document.body.innerHTML = BillsUI({ data: bills });
+
+    const handleClickNewBill = jest.fn(() => NEWBILLS.handleClickNewBill());
+    const button = screen.getByTestId("btn-new-bill");
+    button.addEventListener("click", handleClickNewBill);
+    userEvent.click(button);
+    expect(handleClickNewBill).toHaveBeenCalled();
+    handleClickNewBill.mockRestore();
+  });
+});
+
 //Point 5.2.2 [Bug report] - Ajout des tests unitaires et d'intégration - Composant container/Bills
 // test d'intégration GET
 describe("Given I am a user connected as Employee", () => {
@@ -146,7 +188,10 @@ describe("Given I am a user connected as Employee", () => {
     test("fetches bills from mock API GET", async () => {
       localStorage.setItem(
         "user",
-        JSON.stringify({ type: "Employee", email: "a@a" })
+        JSON.stringify({
+          type: "Employee",
+          email: "employee@test.tld",
+        })
       );
       const root = document.createElement("div");
       root.setAttribute("id", "root");
@@ -154,56 +199,56 @@ describe("Given I am a user connected as Employee", () => {
       router();
       window.onNavigate(ROUTES_PATH.Bills);
       await waitFor(() => {
-        expect(screen.getByText("accepted")).toBeTruthy();
-        expect(screen.getAllByText("pending")).toBeTruthy();
-        expect(screen.getAllByText("refused")).toBeTruthy();
+        expect(screen.getByText("Accepté")).toBeTruthy();
+        expect(screen.getAllByText("En attente")).toBeTruthy();
+        expect(screen.getAllByText("Refused")).toBeTruthy();
       });
-      describe("When an error occurs on API", () => {
-        beforeEach(() => {
-          jest.spyOn(mockStore, "bills");
-          Object.defineProperty(window, "localStorage", {
-            value: localStorageMock,
-          });
-          window.localStorage.setItem(
-            "user",
-            JSON.stringify({
-              type: "Employee",
-              email: "a@a",
-            })
-          );
-          const root = document.createElement("div");
-          root.setAttribute("id", "root");
-          document.body.appendChild(root);
-          router();
+    });
+    describe("When an error occurs on API", () => {
+      beforeEach(() => {
+        jest.spyOn(mockStore, "bills");
+        Object.defineProperty(window, "localStorage", {
+          value: localStorageMock,
         });
-        test("fetches bills from an API and fails with 404 message error", async () => {
-          mockStore.bills.mockImplementationOnce(() => {
-            return {
-              list: () => {
-                return Promise.reject(new Error("Erreur 404"));
-              },
-            };
-          });
-          window.onNavigate(ROUTES_PATH.Bills);
-          await new Promise(process.nextTick);
-          const message = await screen.getByText(/Erreur 404/);
-          expect(message).toBeTruthy();
+        window.localStorage.setItem(
+          "user",
+          JSON.stringify({
+            type: "Employee",
+            email: "employee@test.tld",
+          })
+        );
+        const root = document.createElement("div");
+        root.setAttribute("id", "root");
+        document.body.appendChild(root);
+        router();
+      });
+      test("fetches bills from an API and fails with 404 message error", async () => {
+        mockStore.bills.mockImplementationOnce(() => {
+          return {
+            list: () => {
+              return Promise.reject(new Error("Erreur 404"));
+            },
+          };
+        });
+        window.onNavigate(ROUTES_PATH.Bills);
+        await new Promise(process.nextTick);
+        const message = await screen.getByText(/Erreur 404/);
+        expect(message).toBeTruthy();
+      });
+
+      test("fetches messages from an API and fails with 500 message error", async () => {
+        mockStore.bills.mockImplementationOnce(() => {
+          return {
+            list: () => {
+              return Promise.reject(new Error("Erreur 500"));
+            },
+          };
         });
 
-        test("fetches messages from an API and fails with 500 message error", async () => {
-          mockStore.bills.mockImplementationOnce(() => {
-            return {
-              list: () => {
-                return Promise.reject(new Error("Erreur 500"));
-              },
-            };
-          });
-
-          window.onNavigate(ROUTES_PATH.Bills);
-          await new Promise(process.nextTick);
-          const message = await screen.getByText(/Erreur 500/);
-          expect(message).toBeTruthy();
-        });
+        window.onNavigate(ROUTES_PATH.Bills);
+        await new Promise(process.nextTick);
+        const message = await screen.getByText(/Erreur 500/);
+        expect(message).toBeTruthy();
       });
     });
   });
--- Billed-app-FR-Front/src/__tests__/NewBill.js
+++ Billed-app-FR-Front/src/__tests__/NewBill.js
@@ -2,17 +2,82 @@
  * @jest-environment jsdom
  */
 
-import { screen } from "@testing-library/dom"
-import NewBillUI from "../views/NewBillUI.js"
-import NewBill from "../containers/NewBill.js"
+import { screen, waitFor } from "@testing-library/dom";
+import NewBillUI from "../views/NewBillUI.js";
+import NewBill from "../containers/NewBill.js";
+import mockStore from "../__mocks__/store";
+import { localStorageMock } from "../__mocks__/localStorage.js";
+import { ROUTES, ROUTES_PATH } from "../constants/routes.js";
+import router from "../app/Router.js";
+import { bills } from "../fixtures/bills";
 
+jest.mock("../app/store", () => mockStore);
 
-describe("Given I am connected as an employee", () => {
-  describe("When I am on NewBill Page", () => {
-    test("Then ...", () => {
-      const html = NewBillUI()
-      document.body.innerHTML = html
-      //to-do write assertion
+//Point 5.3.1 [Bug report] - Ajout des tests unitaires et d'intégration - Composant container/NewBill
+
+//Point 5.3.2 [Bug report] - Ajout des tests unitaires et d'intégration - Composant container/NewBill
+// test d'intégration POST
+test("POST bill", async () => {
+  localStorage.setItem(
+    "user",
+    JSON.stringify({
+      type: "Employee",
+      email: "a@a",
     })
-  })
-})
+  );
+  const root = document.createElement("div");
+  root.setAttribute("id", "root");
+  document.body.append(root);
+  router();
+  window.onNavigate(ROUTES_PATH.NewBill);
+  await waitFor(() => screen.getAllByText("Envoyer"));
+});
+
+describe("When an error occurs on API", () => {
+  test("POST bill fails with 500 message error", async () => {
+    try {
+      jest.spyOn(mockStore, "bills");
+
+      Object.defineProperty(window, "localStorage", {
+        value: localStorageMock,
+      });
+
+      window.localStorage.setItem(
+        "user",
+        JSON.stringify({
+          type: "Employee",
+          email: "a@a",
+          password: "employee",
+          status: "connected",
+        })
+      );
+
+      window.onNavigate(ROUTES_PATH.NewBill);
+
+      const root = document.createElement("div");
+      root.setAttribute("id", "root");
+      document.body.appendChild(root);
+      router();
+
+      const buttonSubmit = screen.getAllByText("Envoyer");
+      buttonSubmit[0].click();
+
+      mockStore.bills.mockImplementationOnce(() => {
+        return {
+          create: (bill) => {
+            return Promise.reject(new Error("Erreur 500"));
+          },
+        };
+      });
+
+      window.onNavigate(ROUTES_PATH.NewBill);
+      await new Promise(process.nextTick);
+      const message = screen.queryByText(/Erreur 500/);
+      await waitFor(() => {
+        expect(message).toBeTruthy();
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  });
+});
